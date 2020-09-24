bfeb9e1d78662cd86fb1f5f97e0f3f2d85db736e
Umidbek Karimov <uma.karimov@gmail.com>
2020-09-24 11:38:34 +0500

    feat(http): [parseURITemplate] support composite values.

--- packages/http/src/utils/__tests__/parseURITemplate.spec.ts
+++ packages/http/src/utils/__tests__/parseURITemplate.spec.ts
@@ -10,6 +10,39 @@ const specs = [
   ['Extended', extended],
 ] as const;
 
+test('spec content', () => {
+  expect(Object.keys(examples)).toMatchInlineSnapshot(`
+    Array [
+      "Level 1 Examples",
+      "Level 2 Examples",
+      "Level 3 Examples",
+      "Level 4 Examples",
+    ]
+  `);
+  expect(Object.keys(examplesBySection)).toMatchInlineSnapshot(`
+    Array [
+      "3.2.1 Variable Expansion",
+      "3.2.2 Simple String Expansion",
+      "3.2.3 Reserved Expansion",
+      "3.2.4 Fragment Expansion",
+      "3.2.5 Label Expansion with Dot-Prefix",
+      "3.2.6 Path Segment Expansion",
+      "3.2.7 Path-Style Parameter Expansion",
+      "3.2.8 Form-Style Query Expansion",
+      "3.2.9 Form-Style Query Continuation",
+    ]
+  `);
+  expect(Object.keys(extended)).toMatchInlineSnapshot(`
+    Array [
+      "Additional Examples 1",
+      "Additional Examples 2",
+      "Additional Examples 3: Empty Variables",
+      "Additional Examples 4: Numeric Keys",
+      "Additional Examples 5: Explode Combinations",
+    ]
+  `);
+});
+
 for (const [description, spec] of specs) {
   describe(description, () => {
     for (const [title, { variables, testcases }] of Object.entries(spec)) {
