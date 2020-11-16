803343f1e177b0ca9f166090bb3ef52cbc9fb1e0
Umidbek Karimov <uma.karimov@gmail.com>
2020-11-17 00:21:27 +0500

    feat: Move `@babel/runtime` to the `peerDependencies`.

--- .eslintrc.js
+++ .eslintrc.js
@@ -45,20 +45,9 @@ module.exports = {
 
     {
       files: ['**/*.spec.{ts,tsx}', '**/__testutils__/**/*.{ts,tsx}'],
-      extends: ['plugin:@superdispatch/jest'],
+      extends: ['plugin:@superdispatch/ts-jest'],
       rules: {
-        quotes: 'off',
-        '@typescript-eslint/no-namespace': 'off',
-        '@typescript-eslint/no-unsafe-call': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-unsafe-return': 'off',
-        '@typescript-eslint/no-unsafe-assignment': 'off',
-        '@typescript-eslint/no-unsafe-member-access': 'off',
-        '@typescript-eslint/no-non-null-assertion': 'off',
         'import/no-internal-modules': 'off',
-        'import/no-anonymous-default-export': 'off',
-        'import/no-extraneous-dependencies': 'off',
-        'testing-library/prefer-screen-queries': 'off',
       },
     },
   ],
